91d0ee9bdbfb20e7f2bc3c4ca69ab8213d0fee9f
Thomas_Luk <thomas16@uw.edu>
2020-05-26 14:19:57 -0700

    created postAdapter

--- app/src/main/java/com/example/friendzone/GroupFeed.kt
+++ app/src/main/java/com/example/friendzone/GroupFeed.kt
@@ -2,8 +2,12 @@ package com.example.friendzone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.friendzone.model.Post
 
 class GroupFeed : AppCompatActivity() {
+
+    private lateinit var postAdapter: PostAdapter
+    private lateinit var listOfPosts: MutableList<Post>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/friendzone/PostAdapter.kt
+++ app/src/main/java/com/example/friendzone/PostAdapter.kt
@@ -1,10 +1,42 @@
 package com.example.friendzone
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.example.friendzone.model.Post
+import com.squareup.picasso.Picasso
 
-class PostAdapter {
+class PostAdapter(private val initialListOfPosts: List<Post>): RecyclerView.Adapter<PostAdapter.PostViewHolder>() {
 
-    private lateinit var postAdapter: PostAdapter
-    private lateinit var listOfPosts: MutableList<Post>
+    private var listOfPosts: List<Post> = initialListOfPosts.toList()
+
+    // Creates ViewHolder to hold reference of the views
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.post, parent, false)
+        return PostViewHolder(view)
+    }
+
+    override fun getItemCount() = listOfPosts.size
+
+    override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+        val post: Post = listOfPosts[position]
+        holder.bind(post)
+    }
+
+    inner class PostViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        private val tvUsername = itemView.findViewById<TextView>(R.id.tvUsername)
+        private val ivPostImage = itemView.findViewById<ImageView>(R.id.ivPostImage)
+        private val tvTimestamp = itemView.findViewById<TextView>(R.id.tvTimestamp)
+
+        fun bind(post: Post) {
+            tvUsername.text = post.user
+            Picasso.get().load(post.image).into(ivPostImage)
+            tvTimestamp.text = post.timestamp.toString()
+        }
+    }
+
 }
 
