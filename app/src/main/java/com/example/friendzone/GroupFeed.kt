9aef0d762a140417b1af35bae80c14ca140a9cde
Thomas_Luk <thomas16@uw.edu>
2020-05-26 15:51:54 -0700

    added listOfPosts sample data

--- app/src/main/java/com/example/friendzone/GroupFeed.kt
+++ app/src/main/java/com/example/friendzone/GroupFeed.kt
@@ -3,6 +3,7 @@ package com.example.friendzone
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.friendzone.model.Post
+import kotlinx.android.synthetic.main.activity_group_feed.*
 
 class GroupFeed : AppCompatActivity() {
 
@@ -12,5 +13,10 @@ class GroupFeed : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_group_feed)
+
+        val listOfPosts =
+        val postAdapter = postAdapter(listOfPosts)
+
+        rvPostFeed.adapter = postAdapter
     }
 }
