c93bf7e4ecc908de9d592f5ca2c2366428cf934c
KJTx1 <jasonyanzhizhen@gmail.com>
2020-05-28 12:51:30 -0700

    Main activity import clean up

--- app/src/main/java/com/example/friendzone/MainActivity.kt
+++ app/src/main/java/com/example/friendzone/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.friendzone.UserContent.Companion.USER_DISPLAY_NAME
 import com.example.friendzone.UserContent.Companion.USER_KEY
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
